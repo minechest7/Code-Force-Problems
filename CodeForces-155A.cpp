7761be2f372bb6627f5d70df0600adfa717a224b
Ragib Nihal <minechest7@gmail.com>
2025-09-14 19:40:29 +0600

    Update CodeForces-155A.cpp

--- CodeForces-155A.cpp
+++ CodeForces-155A.cpp
@@ -1,1 +1,53 @@
 //https://codeforces.com/problemset/problem/155/A
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int number;//the number of contest the coder perticipated in
+    cin>>number;
+
+    int points[number];//number of points earned in each contest
+
+    int amazingPerformance=0;
+
+    int mavalue;//maximum value of points at the contest in given time
+    int mivalue;//minimum value of points at the contest in given time
+
+    //counting the number of amazing performances
+    for(int i=0;i<number;i++)
+    {
+        cin>>points[i];
+        if(i==0)
+        {
+        mavalue=points[i];
+        mivalue=points[i];
+        }
+
+
+        if(i!=0)
+        {
+            for(int j=0;j<=i;j++)
+            {
+
+                if(points[j]>mavalue)
+                {
+                    amazingPerformance++;
+                    mavalue=points[j];
+                }
+
+            else if(points[j]<mivalue)
+                {
+                    amazingPerformance++;
+                    mivalue=points[j];
+                }
+            }
+        }
+
+    }
+
+    cout<<amazingPerformance<<endl;
+
+    return 0;
+}
+
