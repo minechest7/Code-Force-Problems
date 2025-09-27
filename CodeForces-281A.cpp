e7b3060fe52586d80d9b7a14d7b54345d4401352
Ragib Nihal <minechest7@gmail.com>
2025-09-27 09:53:24 +0600

    Create CodeForces-281A.cpp

---
+++ CodeForces-281A.cpp
@@ -0,0 +1,17 @@
+//https://codeforces.com/problemset/problem/281/A
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+    string word;
+    cin>>word;
+
+    word[0]=toupper(word[0]);
+
+    cout<<word<<endl;
+
+    return 0;
+}
